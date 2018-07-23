2fe23cdb2f96893addd7a387a9e2ab54d35154d4
Noppawat <noppwvong@me.com>
2018-07-23 16:31:48 +0700

    Initial project

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+const morgan = require('morgan');
+const request = require('request');
 const app = express()
 const port = process.env.PORT || 4000
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(morgan('combined'))
+app.get('/webhook', (req, res) => res.sendStatus(200))
+app.listen(port);
+console.log('Server is listening on ' + port);
